982c63dcb736a170ced037057b3ccaf47cf40451
Misha <bilan.md72@gmail.com>
2023-04-11 21:06:24 +0500

    calc

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int ticket = 15110; // стоимость билета
-        int oneMile = 20; // стоимость одной мили
-
-        int mile = (ticket / oneMile);
-        System.out.println(mile);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
